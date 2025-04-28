79faaaa79f57f1f316cdb408606dd0821474f9f9
caffy07 <dhritivaidya7@gmail.com>
2025-04-28 19:18:19 +0530

    Add files via upload

---
+++ Lowercase.java
@@ -0,0 +1,9 @@
+// Write a Java program to convert a string to lowercase.
+
+public class Lowercase{
+    public static void main(String[] args){
+        String str = "HELLO WORLD";
+        str = str.toLowerCase();
+        System.out.println(str);
+    }
+}
